4d9b5fcf8600f74add6c59b55c46bd0196485b04
nadezda staselovich <staselovichn@mail.ru>
2023-01-19 15:09:23 +0200

    add commit task 4

---
+++ task4/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int i = 1;
+
+while (i <= num)
+{
+  if (i % 2 == 0)
+  {
+    Console.WriteLine(i);
+  }
+  i++;
+}
